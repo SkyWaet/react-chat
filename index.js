c8e6f67bb2482f224b168d9fbdb0a866ba310207
Gleb <gslepenkov@gmail.com>
2021-01-19 13:31:41 +0300

    First Backend version created

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server,{
-  cors:{
+  cors:[{
     origin: "http://localhost:3000",
     methods: ["GET","POST"]
-  }
+  },{
+    origin: "http://192.168.0.213:3000",
+    methods: ["GET","POST"]
+  }]
 });
 
 
@@ -29,8 +32,8 @@ io.on('connect', (socket) => {
 
     socket.join(user.room);
 
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
-    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+    socket.emit('message', { user: 'system', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'system', text: `${user.name} has joined!` });
 
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
@@ -40,16 +43,19 @@ io.on('connect', (socket) => {
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
 
-    io.to(user.room).emit('message', { user: user.name, text: message });
+    io.to(user.room).emit('message', { user: user.name, text: message, date: new Intl.DateTimeFormat('en-Us', { year: "numeric", month: 'short', day: '2-digit' ,hour:'numeric',minute:'numeric'}).format(Date.now()) });
 
     callback();
   });
+  
 
+  
   socket.on('disconnect', () => {
+
     const user = removeUser(socket.id);
 
     if(user) {
-      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('message', { user: 'system', text: `${user.name} has left.` });
       io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
     }
   })
