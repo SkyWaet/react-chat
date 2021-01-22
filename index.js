06d73e039ac280fba8815f65cf4321d4e60f265b
Gleb <gslepenkov@gmail.com>
2021-01-22 18:42:23 +0300

    Share button modified

--- index.js
+++ index.js
@@ -10,32 +10,33 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server,{
-  cors:[{
+const io = socketio(server, {
+  cors: [{
     origin: "http://localhost:3000",
-    methods: ["GET","POST"]
-  },{
+    methods: ["GET", "POST"]
+  }, {
     origin: "http://192.168.0.213:3000",
-    methods: ["GET","POST"]
+    methods: ["GET", "POST"]
   }]
 });
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/',(req,res)=>{
-    res.sendFile('index.html',path.join(__dirname, 'build'));
+app.get('/', (req, res) => {
+  res.sendFile('index.html', path.join(__dirname, 'build'));
 })
+
 app.use(cors());
 
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
-    if(error) return callback(error);
+    if (error) return callback(error);
 
     socket.join(user.room);
 
-    socket.emit('message', { user: 'system', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.emit('message', { user: 'system', text: `${user.name}, welcome to room ${user.room}.` });
     socket.broadcast.to(user.room).emit('message', { user: 'system', text: `${user.name} has joined!` });
 
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
@@ -46,20 +47,20 @@ io.on('connect', (socket) => {
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
 
-    io.to(user.room).emit('message', { user: user.name, text: message, date: new Intl.DateTimeFormat('en-Us', { year: "numeric", month: 'short', day: '2-digit' ,hour:'numeric',minute:'numeric'}).format(Date.now()) });
+    io.to(user.room).emit('message', { user: user.name, text: message, date: new Intl.DateTimeFormat('en-Us', { year: "numeric", month: 'short', day: '2-digit', hour: 'numeric', minute: 'numeric' }).format(Date.now()) });
 
     callback();
   });
-  
 
-  
+
+
   socket.on('disconnect', () => {
 
     const user = removeUser(socket.id);
 
-    if(user) {
+    if (user) {
       io.to(user.room).emit('message', { user: 'system', text: `${user.name} has left.` });
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
     }
   })
 });
